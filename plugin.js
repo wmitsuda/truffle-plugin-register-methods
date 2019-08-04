b7f2a59bbf9d9fe475b0dcf6f52ac312297b5907
Willian Mitsuda <wmitsuda@gmail.com>
2019-08-04 09:58:55 -0700

    Calculating entire function selector

--- plugin.js
+++ plugin.js
@@ -1,3 +1,8 @@
+const Web3 = require("web3");
+const web3 = new Web3(
+  "https://mainnet.infura.io/v3/26572f44518b42b4b1fd44ccfe9a5192"
+);
+
 module.exports = async config => {
   console.log(config);
   console.log("*****************************");
@@ -22,6 +27,22 @@ module.exports = async config => {
         console.log(`${f.name} => constant, skipping`);
       } else {
         console.log(`${f.name} => processing`);
+
+        const signature = buildMethodSignature(f);
+        const selector = web3.utils.keccak256(signature).substring(0, 10);
+        console.log(`\tSignature: ${signature}`);
+        console.log(`\tSelector: ${selector}`);
       }
     });
+};
+
+const buildMethodSignature = f => {
+  let params = "";
+  f.inputs.forEach((input, idx) => {
+    if (idx > 0) {
+      params = params.concat(",");
+    }
+    params = params.concat(input.type);
+  });
+  return `${f.name}(${params})`;
 };
