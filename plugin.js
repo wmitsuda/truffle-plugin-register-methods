358ef0625ac3a3c1d48d80300f940224fe7c2af4
Willian Mitsuda <wmitsuda@gmail.com>
2019-08-04 09:44:54 -0700

    Scanning all functions

--- plugin.js
+++ plugin.js
@@ -1,3 +1,27 @@
 module.exports = async config => {
-  console.log("test");
+  console.log(config);
+  console.log("*****************************");
+
+  // TODO: validate missing contract name
+  const contractName = config._[1];
+  console.log(`Contract name: ${contractName}`);
+
+  const buildDirectory = config.contracts_build_directory;
+  const contractFile = `${buildDirectory}/${contractName}.json`;
+  console.log(`Reading contract file: ${contractFile}`);
+
+  const contractJson = require(contractFile);
+  const { abi } = contractJson;
+  // console.log(abi);
+
+  console.log("\nGetting contract functions...");
+  abi
+    .filter(f => f.type === "function")
+    .forEach(f => {
+      if (f.constant) {
+        console.log(`${f.name} => constant, skipping`);
+      } else {
+        console.log(`${f.name} => processing`);
+      }
+    });
 };
